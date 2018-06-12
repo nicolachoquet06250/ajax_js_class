90b50ad892ba924a6b896d8d039f9d05d17eebfb
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-06-12 17:04:17 +0200

    classe ajax

--- js/Ajax.js
+++ js/Ajax.js
@@ -3,22 +3,48 @@
 class Ajax {
 	constructor(url = '', http_method = 'get', args = {}) {
 		this.url = url;
-		this.http_method = http_method;
+		this.http_method = http_method.toUpperCase();
 		this.args = args;
 	}
 
-	request(async = false) {
-		console.log(this.url);
-		console.log(this.http_method);
-		console.log(this.args);
+	request(functions = {}) {
+		let local_request = new XMLHttpRequest();
+
+		local_request.open(this.http_method.toUpperCase(), this.url);
+
+		local_request.send();
+
+		local_request.onreadystatechange = () => {
+			let return_params = {
+				status: local_request.status,
+				response: {
+					get: (type = '') => {
+						switch (type) {
+							case 'text':
+								return local_request.responseText;
+							case 'xml':
+								return local_request.responseXML;
+							case 'json':
+								return JSON.parse(local_request.responseText);
+							default:
+								return local_request.response;
+						}
+					},
+					ready_state: local_request.readyState,
+					type: local_request.responseType,
+					headers: local_request.getAllResponseHeaders()
+				}
+			};
+
+			if (local_request.readyState === 4 && local_request.status === 200) {
+				if(functions.success) {
+					return functions.success(return_params);
+				}
+			}
+			if(functions.fail) {
+				return functions.fail(return_params);
+			}
+		};
+
 	}
 }
-
-new Ajax(
-	'http://localhost/rest/HelloWorld/test/',
-	'get',
-	[
-		{test1: 'val'},
-		{test: 'toto'}
-	]
-).request();
